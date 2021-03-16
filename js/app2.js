a878eb2ac67ae1614935e63817eb741a2ba8428c
AlejandroNav <alejugger@gmail.com>
2021-03-16 11:37:57 -0600

    using this in random color

--- js/app2.js
+++ js/app2.js
@@ -1,14 +1,23 @@
-const boto = document.querySelector("button")
-boto.addEventListener('click', function(){
-    const nuevoColor = randoColor()
-    document.body.style.backgroundColor = (nuevoColor)
-    const hache1 = document.querySelector('h1')
-    hache1.innerText= nuevoColor
-})
-
 const randoColor = () =>{
     const r = Math.floor(Math.random()*255)
     const g = Math.floor(Math.random()*255)
     const b = Math.floor(Math.random()*255)
     return `rgb(${r},${g},${b})`
+}
+const boto = document.querySelector("#backButton")
+boto.addEventListener('click', function(){
+    document.body.style.backgroundColor = randoColor()
+})
+const allBoton = document.querySelectorAll(".colo")
+for (const iterator of allBoton) {
+    iterator.addEventListener("click",colorize)
+}
+const allH2s = document.querySelectorAll("h2")
+for (const iterator of allH2s) {
+    iterator.addEventListener("click",colorize)
+}
+function colorize(){
+    const nuevoColor = randoColor()
+    this.style.backgroundColor = nuevoColor;
+    this.innerText= nuevoColor;
 }
