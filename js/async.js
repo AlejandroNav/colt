dad490cdd62edddca8578438d4f4f8c8c2f17fcc
Alex-Nav <alejugger@gmail.com>
2021-03-26 17:22:26 -0600

    more

---
+++ js/async.js
@@ -0,0 +1,21 @@
+
+const delayColor = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.background = newColor;
+    }, delay)
+}
+
+delayColor("olive", 600)
+
+let colors = ["Chartreuse", "FireBrick", "LightSalmon", "OliveDrab", "Snow",
+    "Chocolate", "FloralWhite", "LightSeaGreen", "Orange", "SpringGreen",
+    "Coral", "ForestGreen", "LightSkyBlue", "OrangeRed", "SteelBlue",
+    'CornFlowerBlue', "Fuchsia", "LightSlateGray", "Orchid", "Tan",
+    "Cornsilk", "Gainsboro", "LightSteelBlue", "PaleGoldenRod", "Teal"]
+
+
+    for (const key of colors) {
+        delayColor(key, 600)
+        console.log(key);
+    }
+    
