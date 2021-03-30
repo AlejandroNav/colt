a81a6b07a41f11c5bb9834a4c69ca57899d8842b
Alex-Nav <alejugger@gmail.com>
2021-03-29 19:25:50 -0600

    async

--- js/async.js
+++ js/async.js
@@ -22,8 +22,21 @@ for (let i = 0, p = Promise.resolve(); i < 24; i++) {
     p = p.then(_ => new Promise(resolve =>
         setTimeout(function () {
             document.body.style.backgroundColor = colors[i];
-            nombre.innerText=colors[i]
+            nombre.innerText = colors[i]
             resolve();
-        }, 1600)
+        }, 100)
     ));
 }
+
+const helloW = async () => {
+    throw new Error('errar es de humanos')
+    return "Hola que onda amigos"
+}
+
+helloW()
+.then(data =>{
+    console.log("Promesa resuelta con: ", data)
+})
+.catch(err =>{
+    console.log("Promesa recehzada con: ", err)
+})
