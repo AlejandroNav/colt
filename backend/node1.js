503efe0320c34277717d6ec43dc3ffa4ab95954f
Alex-Nav <alejugger@gmail.com>
2021-04-09 12:56:40 -0500

    finish node inro

--- backend/node1.js
+++ backend/node1.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
+
 console.log("hola");
 const args = process.argv.slice(2)
 
 for (let arg of args) {
     console.log('Mi color favorito es ' + arg)
 }
+
+fs.mkdir ('Dogs', {recursive:true},(err) =>{
+    console.log("Inside the Callback");
+    if (err) {
+        throw err;
+    }
+});
+console.log("after mkdir !!!!!!!");
+
+fs.mkdirSync(args[1]);
