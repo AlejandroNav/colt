3cd85596f2c21271ae5a07f1b2d8543f48a523ae
Alex-Nav <alejugger@gmail.com>
2022-05-24 23:58:11 -0500

    starting colt DOM

--- DOM/EventBasics/app.js
+++ DOM/EventBasics/app.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector('#v2');
 
-btn.onclick = function () {
+btn.onclick = function() {
     console.log("YOU CLICKED ME!")
     console.log("I HOPE IT WORKED!!")
 }
@@ -19,13 +19,14 @@ document.querySelector('h1').onclick = () => {
 
 
 const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
+btn3.addEventListener('click', function() {
     alert("CLICKED!");
 })
 
 function twist() {
     console.log("TWIST!")
 }
+
 function shout() {
     console.log("SHOUT!")
 }
