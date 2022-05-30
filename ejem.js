7a6ccede73d8d4b3503772096a2a7205580f0cca
Alex-Nav <alejugger@gmail.com>
2022-05-30 12:23:05 -0500

    boilerplate creat pages

--- ejem.js
+++ ejem.js
@@ -1,4 +1,25 @@
-for (let index = 0; index < 15; index++) {
+const { log } = require('console');
+const fs = require('fs')
+
+for (let index = 0; index < 5; index++) {
     console.log([index]);
 
 }
+const foldername = process.argv[2] || "Project"
+const htmlname = process.argv[3] || "index"
+const cssname = process.argv[4] || "style"
+const jsname = process.argv[5] || "script"
+
+
+//fs.mkdirSync(foldername)
+let ht = (`${foldername}/${htmlname}.html`);
+let cs = (`${foldername}/${cssname}.css`);
+let js = (`${foldername}/${jsname}.js`);
+
+fs.mkdirSync(foldername)
+
+fs.writeFileSync(ht, '')
+
+fs.writeFileSync(cs, '')
+
+fs.writeFileSync(js, '')
