e2bf4b314a5fad185cf35d1d1a8b748c2a2f2467
Alex-Nav <alejugger@gmail.com>
2022-06-01 00:09:21 -0500

    havent finished colt express section

--- expressTuto/index.js
+++ expressTuto/index.js
@@ -1,6 +1,22 @@
 const express = require("express")
 const app = express()
-console.dir(app)
+
+//app.use((req, res) => {
+//console.log("I got a request");
+//    res.send('<h1> HOLA PAGINA </h1>')
+//})
+app.get('/', (req, res) => {
+    console.log("I got a request");
+    res.send('<h3>This is the root  </h3>')
+})
+app.get('/cats', (req, res) => {
+    console.log("I got a request");
+    res.send('<h1> HOLA PAGINA MEOW </h1>')
+})
+app.get('/dogs', (req, res) => {
+    console.log("I got a request");
+    res.send('<h1> HOLA PAGINA WOOF</h1>')
+})
 
 app.listen(3000, () => {
     console.log('Listening in 3000');
