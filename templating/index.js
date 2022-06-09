1aec1bba63eaf6a2871ee01429aabd47715dd1ac
Alex-Nav <alejugger@gmail.com>
2022-06-09 00:55:03 -0500

    add random ejs

--- templating/index.js
+++ templating/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const path = require('path')
 
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (request, response) => {
     //response.sendFile(__dirname + '/index.html')
-    response.send('examplewithout html')
+    response.render('home.ejs')
         //console.log('this is the request from main page', request);
+})
+
+app.get('/rand', (req, res) => {
+    res.render('random')
 })
 
 app.listen(3000, () => {
